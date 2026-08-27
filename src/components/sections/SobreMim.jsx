f5f2607c4f68ddd7cf6d95a1e670560668be19c7
zero0o2 <carlosbatata195@gmail.com>
2026-08-27 13:29:59 -0300

    about me

--- src/components/sections/SobreMim.jsx
+++ src/components/sections/SobreMim.jsx
@@ -25,7 +25,7 @@ const SobreMim = () => {
                         </header>
                         <span className="text-[15px] flex flex-col gap-[10px]">
                             <p>Sou um jovem de 18 anos buscando ingressar no mercado de trabalho de desenvolvimento de softwares, estudo tecnologia desde 2022 que foi quando começei a ter gosto pela area.</p>
-                            <p>Em 2023 ingressei em uma formaçao da escola de tecnologia <strong>Digital College</strong>, aonde cursei e completei em um ano e meio desenvolvimento FullStak com javascript, aonde tive conteudos como javascript, Node.js, Express, MySQL, Mongodb, React, trabalho em equipe e vercionamento com git.</p>
+                            <p>Em 2023 ingressei em uma formaçao da escola de tecnologia <strong>Digital College</strong>, aonde cursei e completei em um ano e meio desenvolvimento FullStak com javascript, aonde tive conteudos como javascript, Node.js, Express, MySQL, Mongodb, React, trabalho em equipe e vercionamento c   om git.</p>
                             <p>Atualmente em 2026 estou cursando a faculdade de Analise e Desenvolvimento de Sistemas(ADS) pela Estacio 1° periodo</p>
                         </span>
                         <nav className="flex flex-col gap-[20px]">
@@ -43,14 +43,16 @@ const SobreMim = () => {
                             <header className="flex flex-col gap-[10px]">
                                 <h1 className="text-[20px] ">Certificações e competências</h1>
                             </header>
-                            <section className="w-full flex justify-center p-[20px] gap-[14px]">
-                                <span className="h-full flex-1 max-w-[450px] hover:bg-[var(--cor02)] cursor-pointer flex flex-col">
-                                    <img className="object-cover h-[200px] duration-200" src="/imagens/certificacoes/digitalCollege.png" alt="" />
+                            <section className="w-full flex overflow-x-auto p-[20px] gap-[20px]">
+
+                                <span className="flex-1 min-w-[350px] max-w-[450px] shadow-[0_0_20px_var(--cor01)] hover:bg-[var(--cor02)] cursor-pointer flex flex-col">
+                                    <img className="object-cover object-center h-[200px] duration-200" src="/imagens/certificacoes/digitalCollege.png" alt="" />
                                     <div className="p-[20px] text-[15px] flex flex-col gap-[10px]">
                                         <h1 className="text-[17px] font-bold" >Certificado de conclusao - Digital College</h1>
                                         <p>O curso de Formação Full-Stack (com foco em JavaScript e tecnologias modernas) da Digital College prepara você para atuar no desenvolvimento completo de sites e aplicativos, desde a interface visual até os servidores e bancos de dados.</p>
                                     </div>
                                 </span>
+                                
                             </section>
                         </div>
                     </div>
